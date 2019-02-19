ff782cff3f67ea5500483bf8d81489adc6f0eff6
Pierre Cariou <pierrecariou@outlook.fr>
2019-02-19 15:25:16 +0100

    profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
