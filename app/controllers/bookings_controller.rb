f57e9292b09ab894db3d00678f217575af32d8ee
Matthieu Montagne <matthieumontagne@gmail.com>
2019-02-19 12:29:05 +0100

    artworks/booking OK

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,42 +1,18 @@
 class BookingsController < ApplicationController
-  def index
-    @bookings = Booking.all
-  end
-
-  def show
-    @booking = Booking.find(params[:id])
-  end
-
   def new
+    @artwork = Artwork.find(params[:artwork_id])
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
-    if @booking.save
-      redirect_to bookings_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @booking = Booking.find(params[:id])
-  end
-
-  def update
-    @booking = Booking.find(params[:id])
-    @booking.update(booking_params)
-  end
-
-  def destroy
-    @booking = Booking.find(params[:id])
-    @booking.destroy
+    @booking.artwork = Artwork.find(params[:artwork_id])
+    @booking.save
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:status)
+    params.require(:booking).permit(:content)
   end
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,5 +1,5 @@
 class Artwork < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true
-
+  has_many :bookings
 end
