66bf4cd87f05fc40486978509dd2c95774d95213
Theodore Brossollet <theodore.brossollet@edu.emlyon.com>
2019-02-20 11:53:23 +0100

    show artwork done

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,12 @@ url6 ='https://payload.cargocollective.com/1/22/726497/12334185/infinitions--2_1
 
 puts 'Creating artworks...'
 
-cyclique = Artwork.new(name: 'Débordement cyclique', category: 'gravure', user_id: 1, availability: 'free')
-bienheureux = Artwork.new(name:'77 bienheureux et leur conscience collective', category:'sculpture', user_id: 1, availability: 'free')
-magnesia = Artwork.new(name:'Magnésia', category: 'sculpture', user_id: 1, availability: 'free')
-revele = Artwork.new(name: 'Révèle', category: 'Peinture', user_id: 1, availability: 'free')
-trio = Artwork.new(name: 'trie eau', category: 'Peinture', user_id: 1, availability: 'free')
-infinition = Artwork.new(name: 'infinition', category: 'sculpture', user_id: 1, availability: 'free')
+cyclique = Artwork.new(name: 'Débordement cyclique', category: 'gravure', user_id: 1, availability: 'disponible')
+bienheureux = Artwork.new(name:'77 bienheureux', category:'sculpture', user_id: 1, availability: 'disponible')
+magnesia = Artwork.new(name:'Magnésia', category: 'sculpture', user_id: 1, availability: 'disponible')
+revele = Artwork.new(name: 'Révèle', category: 'Peinture', user_id: 1, availability: 'disponible')
+trio = Artwork.new(name: 'trie eau', category: 'Peinture', user_id: 1, availability: 'disponible')
+infinition = Artwork.new(name: 'infinition', category: 'sculpture', user_id: 1, availability: 'disponible')
 
 
 cyclique.remote_photo_url = url1
