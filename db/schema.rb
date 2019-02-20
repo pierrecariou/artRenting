128341148a931afaf60d52d14996ea12a126b25c
Theodore Brossollet <theodore.brossollet@edu.emlyon.com>
2019-02-20 12:59:06 +0100

    newartworkshow

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_02_19_145249) do
-
+ActiveRecord::Schema.define(version: 2019_02_19_153841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_02_19_145249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "availability"
-    t.date "date"
     t.date "start_date"
     t.date "end_date"
     t.index ["user_id"], name: "index_artworks_on_user_id"
